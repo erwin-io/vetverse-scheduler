315e61189bf76e526233c0b5e763ec49cfbc38f4
erwin-io <erwinramirez220@gmail.com>
2022-12-19 00:15:11 +0800

    updates

---
+++ announcements.scheduler.js
@@ -0,0 +1,27 @@
+
+var request = require('request');
+if(process.env.NODE_ENV && process.env.NODE_ENV === "dev") {
+    require("dotenv").config({ path: "development.env"});
+}
+else {
+    require("dotenv").config();
+}
+
+const apiEndpoint = `${process.env.SCHEDULERAPIURL}/runAnnouncements`;
+const post = async function() {
+    return await request.post(
+        apiEndpoint,
+        async function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                return {
+                    error,
+                    body,
+                    response
+                };
+            }
+        }
+    );
+};
+post().then(res => {
+    // console.log('response ', res);
+})
--- index.js
+++ index.js
@@ -1,21 +1,2 @@
 
-var request = require('request');
 
-
-const post = async function() {
-    return await request.post(
-        process.env.SCHEDULERAPIURL,
-        async function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                return {
-                    error,
-                    body,
-                    response
-                };
-            }
-        }
-    );
-};
-post().then(res => {
-    console.log('response ', res);
-})
